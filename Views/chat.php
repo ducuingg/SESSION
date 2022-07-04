71eaec1dfcfaeed6ed30d2f99845dd5e6da059db
ducuingg <ducuingg@yahoo.fr>
2022-07-04 11:00:39 +0200

    modifs

---
+++ Views/chat.php
@@ -0,0 +1,6 @@
+<h5>Chat</h5>
+<form action="" method="POST">
+    <input type="text" name="pseudo" id="pseudo" placeholder="PSEUDO"><br>
+    <input type="textarea" name="name" id="name" placeholder="Message"><br>
+    <input type="submit" value="Envoyer">
+</form>
